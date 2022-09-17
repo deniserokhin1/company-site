cb81f0b778935bc02db141272934dd06b82e2df8
Denis <oterohina@gmail.com>
2022-09-17 13:27:06 +0300

    Updates

---
+++ main.a4523a0c90b0b26b4cf8.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const t=document.querySelector(".about__play"),e=document.querySelector(".about__video video");t.addEventListener("click",(()=>{e.play(),e.setAttribute("controls","controls"),t.setAttribute("hidden",!0)}))})();
